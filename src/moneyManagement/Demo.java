df354ae40c160230ff6adb2fa6c9e4b95e1a87b1
Prateek Budhiraja <36406919+prateek1408@users.noreply.github.com>
2020-02-25 15:34:37 +0530

    Add files via upload

--- src/moneyManagement/Demo.java
+++ src/moneyManagement/Demo.java
@@ -1,9 +1,53 @@
 package moneyManagement;
 
 public class Demo {
+	private String demoName;
+	public void startUp() {
+		System.out.print("Please enter your E-mail id : ");
+		demoName = Alpha.sc.nextLine();
+		
+		//here some code that will search the database that the user is 
+		//present in the database or not..
+		//after that it will ask for password if the user is present, else 
+		//it will ask to create a new account
+		
+		//some code........
+		
+		/*System.out.println("Enter number of members in your family");
+		Alpha.sc.nextInt();
+		...................................
+		*/
+		
+	}
+	public void menu(Person obj) {
+		System.out.println("Menu : ------ ");
+		System.out.println("1. Add an expense");
+		System.out.println("2. Delete an expense");
+		System.out.println("3. Show Your monthly expense");
+		System.out.println("4. Show total monthly expense");
+		System.out.println("Enter your choice : ");
+		int ch = Alpha.sc.nextInt();
+		switch(ch) {
+		case 1  :		
+		case 2  : 
+		case 3  :
+		case 4  :
+			Expense.expMenu(obj);
+		default :
+			System.out.println("Please choose from the available options..");
+			System.out.println("Why not You take one more try to choose...");
+			menu(obj);
+		}
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Person me = new Person();
+		new Demo().startUp();
+		
+		
+		
+		//int choice = menu();
+		
+		
+		/*Person me = new Person();
 		Expense.mobile(me);
 		Expense.food(me);
 		Expense.petrol(me);
@@ -23,7 +67,7 @@ public class Demo {
 		Expense.misc(someone1);
 		someone1.showDetails();
 		Expense.total_exp();
-	
+	*/
 	}
 
 }
--- src/moneyManagement/Expense.java
+++ src/moneyManagement/Expense.java
@@ -1,6 +1,26 @@
 package moneyManagement;
 
 public class Expense {
+	public static void expMenu(Person obj) {			// User can choose what type of expense is this and 
+														//after determining it will transfer to the respected method
+		System.out.println("Menu : ------ ");
+		System.out.println("1. Mobile Expense");
+		System.out.println("2. Food Expense");
+		System.out.println("3. Petrol Expense");
+		System.out.println("4. Miscellaneous Expense");
+		System.out.println("Enter your choice : ");
+		int ch = Alpha.sc.nextInt();
+		switch(ch) {
+		case 1  :		mobile(obj);
+		case 2  : 		food(obj);
+		case 3  :		petrol(obj);
+		case 4  :		misc(obj);
+		default :
+			System.out.println("Please choose from the available options..");
+			System.out.println("Why not You take one more try to choose...");
+			expMenu(obj);
+		}
+	}
 	public static void mobile(Person person, int ...x) {
 		if(x.length == 0) getAmt();
 	}
